3f4a34e9d13de740aff3577d6203595d376fbc4b
Ebin <ebin.cm@pitsolutions.com>
2016-05-03 11:55:56 +0530

    indentation with spaces

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,18 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Pits.' . $_EXTKEY,
-	'Pitswdcalender',
-	array(
-		'EventCalender' => 'list, show, new, create, edit, update, delete,compact',
-		
-	),
-	// non-cacheable actions
-	array(
-		'EventCalender' => 'list, create, update, delete,compact',
-		
-	)
+    'Pits.' . $_EXTKEY,
+    'Pitswdcalender',
+    array(
+        'EventCalender' => 'list, show, new, create, edit, update, delete,compact',
+        
+    ),
+    // non-cacheable actions
+    array(
+        'EventCalender' => 'list, create, update, delete,compact',
+        
+    )
 );
