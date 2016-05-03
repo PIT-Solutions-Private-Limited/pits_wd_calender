9679b728394db548b2d45680957c77f01f8e3a26
Ebin <ebin.cm@pitsolutions.com>
2016-05-03 11:37:24 +0530

    7.6.99 supportive ,converted to namespaces

--- ext_tables.php
+++ ext_tables.php
@@ -1,54 +1,20 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+	die('Access denied.');
 }
 
-Tx_Extbase_Utility_Extension::registerPlugin(
-	$_EXTKEY,
-	'Wdcalender',
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	'Pits.' . $_EXTKEY,
+	'Pitswdcalender',
 	'Event Calender'
 );
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 
-							'Configuration/TypoScript', 
-							'Event Calender');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Event Calender');
 
-t3lib_extMgm::addLLrefForTCAdescr(
-									'tx_pitswdcalender_domain_model_eventcalender',
-									'EXT:pits_wd_calender/Resources/Private/Language/locallang_eventcalender.xml'
-									);
-t3lib_extMgm::allowTableOnStandardPages(
-										'tx_pitswdcalender_domain_model_eventcalender'
-										);
-$TCA['tx_pitswdcalender_domain_model_eventcalender'] = array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:pits_wd_calender/Resources/Private/Language/locallang_db.xml:tx_eventcalender',
-		'label' => 'uid',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => TRUE,
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pitswdcalender_domain_model_eventcalender', 'EXT:pits_wd_calender/Resources/Private/Language/locallang_csh_tx_pitswdcalender_domain_model_eventcalender.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pitswdcalender_domain_model_eventcalender');
 
-		'versioningWS' => 2,
-		'versioning_followPages' => TRUE,
-		'origUid' => 't3_origuid',
-		'languageField' => 'sys_language_uid',
-		'transOrigPointerField' => 'l10n_parent',
-		'transOrigDiffSourceField' => 'l10n_diffsource',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'searchFields' => '',
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) .
-								'Configuration/TCA/EventCalender.php',
-		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 
-								'Resources/Public/Icons/eventcalender.gif'
-	),
-);
-$pluginSignature = str_replace('_', '', $_EXTKEY).'_wdcalender';
+
+$pluginSignature = str_replace('_', '', $_EXTKEY).'_pitswdcalender';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform.xml');
-?>
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform.xml');
