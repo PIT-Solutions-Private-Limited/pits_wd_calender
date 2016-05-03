c61a63f033f13bdac57758571c8ba887166251e2
Ebin <ebin.cm@pitsolutions.com>
2016-05-03 11:53:04 +0530

    indentation with spaces

--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,12 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'Pits.' . $_EXTKEY,
-	'Pitswdcalender',
-	'Event Calender'
+    'Pits.' . $_EXTKEY,
+    'Pitswdcalender',
+    'Event Calender'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Event Calender');
